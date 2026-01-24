fe8219f97a72fbd0b02e8da499725d0f2eca97fa
astarion-ow <recoleta.ow@gmail.com>
2026-01-23 19:59:43 -0600

    avif testing

---
+++ data.js
@@ -0,0 +1,152 @@
+var brig = [{
+    "key": "Health",
+    "value": 200
+}, {
+    "key": "Armor",
+    "value": 50
+}, {
+    "key": "MaxHP",
+    "value": 250
+}, {
+    "key": "Speed",
+    "value": 5.5
+}, {
+    "key": "RegenRate",
+    "value": 22.5
+}];
+
+var flail = [{
+    "key": "Desc",
+    "value": "Melee weapon with extended range."
+}, {
+    "key": "Damage",
+    "value": 45
+}, {
+    "key": "Range",
+    "value": 6
+}];
+
+var pack = [{
+    "key": "Desc",
+    "value": "Heals an ally for a short duration."
+}, {
+    "key": "Cooldown",
+    "value": 5
+}, {
+    "key": "Burst",
+    "value": 25
+}, {
+    "key": "Healing",
+    "value": 100
+}, {
+    "key": "Range",
+    "value": 25
+}, {
+    "key": "Duration",
+    "value": 2
+}, {
+    "key": "Charges",
+    "value": 3
+}];
+
+var whipshot = [{
+    "key": "Desc",
+    "value": "Launch your flail forward to knock an enemy away from you."
+}, {
+    "key": "Cooldown",
+    "value": 4
+}, {
+    "key": "Damage",
+    "value": 70
+}, {
+    "key": "Range",
+    "value": 20
+}, {
+    "key": "Knockback",
+    "value": 25
+}];
+
+var shield = [{
+    "key": "Desc",
+    "value": "Hold secondary fire to deploy a frontal energy barrier."
+}, {
+    "key": "Health",
+    "value": 250
+}, {
+    "key": "RechargeRate",
+    "value": 85
+}, {
+    "key": "MovementPenalty",
+    "value": 30
+}];
+
+var bash = [{
+    "key": "Desc",
+    "value": "Available when Barrier Shield is deployed. Dash forward to knock back an enemy."
+}, {
+    "key": "Cooldown",
+    "value": 5
+}, {
+    "key": "Damage",
+    "value": 70
+}, {
+    "key": "Knockback",
+    "value": 6
+}, {
+    "key": "Speed",
+    "value": 400
+}];
+
+var inspire = [{
+    "key": "Desc",
+    "value": "Dealing damage heals nearby allies."
+}, {
+    "key": "Healing",
+    "value": 15
+}, {
+    "key": "Duration",
+    "value": 4
+}, {
+    "key": "TotalHealing",
+    "value": 60
+}, {
+    "key": "Range",
+    "value": 20
+}];
+
+var rally = [{
+    "key": "Desc",
+    "value": "Gain armor, empower Barrier Shield, and provide extra health to nearby allies."
+}];
+
+var minor_perk_1 = [{
+    "key": "Name",
+    "value": "Combat Medic"
+}, {
+    "key": "Desc",
+    "value": "Melee attacks reduce the cooldown of Repair Pack by 0.5 seconds."
+}];
+
+var minor_perk_2 = [{
+    "key": "Name",
+    "value": "Morale Boost"
+}, {
+    "key": "Desc",
+    "value": "Inspire lasts 3 seconds longer when activated by Whip Shot."
+}];
+
+var major_perk_1 = [{
+    "key": "Name",
+    "value": "Inspiring Strike"
+}, {
+    "key": "Desc",
+    "value": "Shield Bash grants 30% increased movement speed for 2 seconds. Inspire's healing is instant when activated by Shield Bash."
+}];
+
+var major_perk_2 = [{
+    "key": "Name",
+    "value": "Whiplash"
+}, {
+    "key": "Desc",
+    "value": "Melee attacks reduce the cooldown of Repair Pack by 0.5 seconds."
+}];
