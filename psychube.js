b38a09ce6d9cfa7ea3d439b0b991f483426ce15e
astarion-ow <recoleta.ow@gmail.com>
2026-05-26 19:46:41 -0500

    u

--- psychube.js
+++ psychube.js
@@ -4,6 +4,9 @@ const psychubeR = document.querySelector('.psychube-r');
 const psychubeA = document.querySelector('.psychube-a');
 const psychubeL = document.querySelector('.psychube-lly-to-me');
 
+const bubbleR   = document.querySelector('.psychube-circle-r');
+const bubbleA   = document.querySelector('.psychube-circle-a');
+
 const psychubeImage = document.querySelector('.psychube-image');
 
 let targetX = 0;
@@ -35,9 +38,43 @@ function animate(){
     psychubeA.style.transform = `translateX(-50%) translate(${currentX * 14}px, ${currentY * 14}px)`;
     psychubeL.style.transform = `translateX(-50%) translate(${currentX * 9.8}px, ${currentY * 9.8}px)`;
 
+    bubbleR.style.transform = `translateX(-50%) translate(${currentX * 4.2}px, ${currentY * 4.2}px)`;
+    bubbleA.style.transform = `translateX(-50%) translate(${currentX * 12.6}px, ${currentY * 12.6}px)`;
+
     psychubeImage.style.transform = `translate(${currentX * -1.4}px, ${currentY * -1.4}px)`;
 
     requestAnimationFrame(animate);
 }
 
-animate();
+function handleOrientation(event){
+    //left/right tilt
+    const gamma = event.gamma;
+
+    //front/back tilt
+    const beta = event.beta;
+
+    targetX = gamma / 30;
+    targetY = beta / 30;
+}
+
+window.addEventListener(
+    'deviceorientation',
+    handleOrientation
+);
+
+const isMobile =
+    window.DeviceOrientationEvent &&
+    'ontouchstart' in window;
+
+if (isMobile) {
+    window.addEventListener(
+        'deviceorientation',
+        handleOrientation
+    );
+}
+else {
+    card.addEventListener(
+        'mousemove',
+        animate()
+    );
+}
